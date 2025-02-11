b3bbc3df95fcad58e5ba77b227e74ab3a948dfcb
ShikharJoshi98 <shikharjoshi89@gmail.com>
2025-02-11 21:55:10 +0530

    Updated

---
+++ src/assets/assets.js
@@ -0,0 +1,5 @@
+import aristImage from './artists-image.jpg';
+
+export const assets = {
+    aristImage
+}
--- src/components/Artist.jsx
+++ src/components/Artist.jsx
@@ -3,7 +3,7 @@ import Logo from '../public/Logo'
 
 const Artist = () => {
   return (
-    <div className="bg-[url('./src/public/artists-image.jpg')] h-[490px] w-full bg-cover bg-center bg-no-repeat">
+    <div className="bg-[url('/artists-image.jpg')] h-[490px] w-full bg-cover bg-center bg-no-repeat">
         <div className='p-14'>
               <Logo width={218} height={112} color='#056de6' />
         </div>
