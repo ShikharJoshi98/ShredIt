a4e6e32fc7d166ae6447e2cbb650c8117da0d373
ShikharJoshi98 <shikharjoshi89@gmail.com>
2025-02-11 22:00:48 +0530

    Updated

--- src/components/Artist.jsx
+++ src/components/Artist.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Logo from '../public/Logo'
+import artistsImage from '../assets/artists-image.jpg';
 
 const Artist = () => {
   return (
-    <div className="bg-[url('/artists-image.jpg')] h-[490px] w-full bg-cover bg-center bg-no-repeat">
+    <div className="h-[490px] w-full bg-cover bg-center bg-no-repeat"
+    style={{ backgroundImage: `url(${artistsImage})` }}>
         <div className='p-14'>
               <Logo width={218} height={112} color='#056de6' />
         </div>
